71c07d43d159dd202c4a480f53ae146212ffa170
7to4 <vikibornin1991@gmail.com>
2019-02-07 06:21:20 +0100

    feat 43: Email service. Basic workflow with basic test cases

---
+++ src/test/java/acime/MailComposerTest.java
@@ -0,0 +1,31 @@
+package acime;
+
+import acime.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MailComposerTest {
+    /**
+     * Given the correct details, the email must get sent
+     */
+    @Test
+    public void validDetails(){
+        Assertions.assertTrue(new MailComposer().sendEmail("vikibornin1991@gmail.com","From Test fn"));
+    }
+
+    /**
+     * When the recipient info is invalid, no email is sent
+     */
+    @Test
+    public void invalidRecipient(){
+        Assertions.assertFalse(new MailComposer().sendEmail("invalid","From Test fn"));
+    }
+
+    /**
+     * When the body is empty, no email is sent
+     */
+    @Test
+    public void EmptyBody(){
+        Assertions.assertFalse(new MailComposer().sendEmail("invalid",""));
+    }
+}
